84da2953b899efcf7463464230599d3d4c999d14
Johanna2612 <j.rohe@hbk-bs.de>
2025-05-15 12:27:30 +0200

    Co-authored-by: Fabian Morón Zirfas <ff6347@users.noreply.github.com>

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ let baseSpeedTop = 1;
 let baseSpeedMiddle = 0.75;
 let baseSpeedBottom = 0.5;
 
-let numParticles = 1500; // Erhöhe die Anzahl der Partikel für einen dichteren Effekt
+let numParticles = 1400; // Erhöhe die Anzahl der Partikel für einen dichteren Effekt
+let turbulenceFactor = 0.1; // Stärke der Turbulenzen
 
 function setup() {
-  createCanvas(600, 500);
+  createCanvas(600, 600);
   noStroke();
 
   for (let i = 0; i < numParticles; i++) {
     let y = random(height);
     let speed;
-    let alphaValue = random(150, 210);
-    let sizeValue = random(2, 5);
+    let alphaValue = random(1, 4);
+    let sizeValue = random(1, 2);
 
     // Sanfter Übergang der Geschwindigkeit basierend auf der y-Position
     if (y < height / 3) {
@@ -31,30 +32,42 @@ function setup() {
     let densityFactor = map(y, 0, height, 1, 0.3); // Linearer Abfall der Dichte von oben nach unten
     if (random(1) < densityFactor) {
       particles.push({
-        x: random(width),
+        x: random(width) - width,
         y: y,
-        size: 2,
+        size: 2.5 + random(5, 20),
         alpha: alphaValue,
-        speed: speed
+        speed: speed,
+        yVelocity: 0 // Starte mit einer vertikalen Geschwindigkeit von Null
       });
     }
   }
   console.log("Anzahl der Partikel:", particles.length); // Gib die tatsächliche Anzahl der erzeugten Partikel aus
 }
 
-
 function draw() {
-  background(255); // Weißer Hintergrund bleibt
+  background(255, 10); // Weißer Hintergrund bleibt
 
   fill(0); // Graue Farbe für alle Partikel
   for (let i = 0; i < particles.length; i++) {
     let p = particles[i];
-    fill(0, p.alpha); // Verwende die individuelle Alpha-Komponente
+    fill(p.y / 10, p.alpha); // Verwende die individuelle Alpha-Komponente
     ellipse(p.x, p.y, p.size, p.size);
-    p.x += p.speed;
+    p.x += p.speed + noise(p.x * 0.1, frameCount * 0.01) * 0.5; // Füge Rauschen für subtile horizontale Variation hinzu
+    p.yVelocity += random(-turbulenceFactor, turbulenceFactor); // Zufällige vertikale Beschleunigung (Turbulenz)
+    p.y += p.yVelocity; // Anwenden der vertikalen Geschwindigkeit
+    p.yVelocity *= 0.85; // Langsames Abbremsen der vertikalen Geschwindigkeit, um ein zu starkes Auf und Ab zu verhindern
+
+    // Begrenzung der vertikalen Bewegung, falls gewünscht
+    if (p.y < 0) {
+       p.y = 0;
+      p.yVelocity *= -0.8; // Abprallen oder Umkehren
+     } else if (p.y > height) {
+      p.y = height;
+       p.yVelocity *= -0.8; // Abprallen oder Umkehren
+     }
+
     if (p.x > width + p.size / 2) {
       p.x = -p.size / 2;
     }
   }
 }
-
